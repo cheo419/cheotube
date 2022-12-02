04cf567575774e684f2651248459e86c09ead5ba
cheo419 <theater914@gmail.com>
2022-12-02 17:45:13 +0900

    replace

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -61,6 +61,5 @@ export const postLogin = async (req, res) => {
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
