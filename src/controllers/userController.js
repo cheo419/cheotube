1480418ba23025fc085d642d583f79b312fb0bf2
cheo419 <theater914@gmail.com>
2022-12-12 17:46:13 +0900

    User's Videos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -215,14 +215,12 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
     pageTitle: `${user.name}의 프로필`,
     user,
-    videos,
   });
 };
